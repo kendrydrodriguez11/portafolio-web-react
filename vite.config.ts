4c3460dc577915ce8ad47e5420d2a41757a2be5e
kendrydrodriguez11 <kendrydrodriguez6@gmail.com>
2026-01-14 18:36:30 -0500

    corregion se agrego node types

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/portafolio-web-react/',
   plugins: [react()],
   resolve: {
     alias: {
